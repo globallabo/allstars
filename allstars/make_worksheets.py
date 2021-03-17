4bd6db0a2b274f2a650ab5707750e8e22b6852bf
Christopher Bunn <github@thenerdery.org>
2021-03-17 20:47:39 +0900

    Move template reading to a function.

--- allstars/make_worksheets.py
+++ allstars/make_worksheets.py
@@ -1,8 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 # from pprint import pprint
-from weasyprint import HTML, CSS
-from weasyprint.fonts import FontConfiguration
+from weasyprint import HTML
 from string import Template
 import logging
 
@@ -190,6 +189,13 @@ def create_template_mapping(data: list, level: int, unit: int, lesson: int) -> d
     return template_mapping
 
 
+# Open HTML template file and return contents as string
+def get_template(filename: str) -> str:
+    with open(filename, "r") as template_file:
+        template_file_contents = template_file.read()
+    return template_file_contents
+
+
 # Log WeasyPrint output
 logger = logging.getLogger('weasyprint')
 logger.addHandler(logging.FileHandler('/tmp/weasyprint.log'))
@@ -208,14 +214,10 @@ lessons = [1, 2, 3, 4]
 for level in levels:
     print(f'Level {level}')
     # Create HTML template
-    font_config = FontConfiguration()
     template_filename = f'AS{level}-worksheets-template.html'
-    with open(template_filename, "r") as template_file:
-        template_file_contents = template_file.read()
+    # Get contents of HTML template file
+    template_file_contents = get_template(filename=template_filename)
     template_string = Template(template_file_contents)
-    css_filename = "worksheets.css"
-    with open(css_filename, "r") as css_file:
-        css_string = css_file.read()
 
     # output_path = f'/Users/cbunn/Documents/Employment/5 Star/Google Drive/All Stars Second Edition/All Stars Second Edition/Worksheets/Level {level}/'
     # output_path = f'/Users/cbunn/Documents/Employment/5 Star/Google Drive/All Stars Second Edition/unit1-output/Level {level}/'
