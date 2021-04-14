3ad35a618f3a8acb0cf1ccde72ace2899bf492f4
Christopher Bunn <github@thenerdery.org>
2021-04-14 20:42:07 +0900

    Update image downloading script for Windows using selenium and a chromedriver, but it isn't working.

--- allstars/get_images.py
+++ allstars/get_images.py
@@ -5,19 +5,22 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from zipfile import ZipFile
+from dotenv import load_dotenv
 
-output_path = "/Users/cbunn/Documents/Employment/5 Star/Google Drive/All Stars Second Edition/images-uncropped/Level 2"
+load_dotenv()  # take environment variables from .env.
+
+output_path = "/mnt/c/Use/c/Doc/Go/All Stars Second Edition/images-uncropped/Level 4/"
 
 # Set up Selenium Chrome Webdriver Options
 webdriver_options = Options()
-# webdriver_options.headless = True
+webdriver_options.headless = True
 webdriver_options.add_experimental_option('prefs', {
     "download.default_directory": output_path,
     "download.prompt_for_download": False,
     "download.directory_upgrade": True})
-webdriver_path = "/usr/local/bin/chromedriver"
-driver = webdriver.Chrome(webdriver_path, options=webdriver_options)
 
+webdriver_path = "/usr/bin/chromedriver"
+driver = webdriver.Chrome(webdriver_path, options=webdriver_options)
 # Freepik info
 login_url = 'https://www.freepik.com/profile/login'
 username = os.environ.get('FREEPIK_USERNAME')
@@ -32,9 +35,11 @@ time.sleep(120)
 
 # levels = [1, 2, 3]
 # units = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-levels = [2]
-units = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-lessons = [1, 2, 3, 4]
+levels = [4]
+# units = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+units = [1]
+# lessons = [1, 2, 3, 4]
+lessons = [1]
 
 for level in levels:
     print(f'Level {level}')
@@ -45,11 +50,7 @@ for level in levels:
              "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
     client = gspread.authorize(creds)
-    # *** This is only necessary until the Level 2 old/new sheets get merged ***
-    if level == 1:
-        sheet = client.open("all_stars_revised_0128").get_worksheet(level - 1)
-    else:
-        sheet = client.open("all_stars_revised_0128").get_worksheet(level)
+    sheet = client.open("all_stars_revised_0128").worksheet(f"Level {level}")
     data = sheet.get_all_values()
 
     # Set the starting point of the gspread output
@@ -76,24 +77,27 @@ for level in levels:
                 still_downloading = True
                 while still_downloading:
                     time.sleep(10)
-                    zip_filename = max(
+                    filename = max(
                         [output_path + f for f in os.listdir(output_path)],
                         key=os.path.getctime)
-                    if zip_filename.endswith('.zip'):
+                    if filename.endswith('.jpg'):
                         still_downloading = False
-                with ZipFile(zip_filename, 'r') as zip_object:
-                    # Get a list of all archived file names from the zip
-                    list_of_filenames = zip_object.namelist()
-                    # Iterate over the file names
-                    for filename in list_of_filenames:
-                        # find the JPG image
-                        if filename.endswith('.jpg'):
-                            # get the info on the JPG file
-                            file_info = zip_object.getinfo(filename)
-                            # rename the JPG
-                            file_info.filename = f"AS{level}U{unit:02}L{lesson:02}-story.jpg"
-                            # Extract the JPG file from the zip
-                            zip_object.extract(file_info, output_path)
+                new_filename = f"AS{level}U{unit:02}L{lesson:02}-story.jpg"
+                os.rename(filename, new_filename)
+                # *** Freepik now sends JPGs
+                # with ZipFile(zip_filename, 'r') as zip_object:
+                #     # Get a list of all archived file names from the zip
+                #     list_of_filenames = zip_object.namelist()
+                #     # Iterate over the file names
+                #     for filename in list_of_filenames:
+                #         # find the JPG image
+                #         if filename.endswith('.jpg'):
+                #             # get the info on the JPG file
+                #             file_info = zip_object.getinfo(filename)
+                #             # rename the JPG
+                #             file_info.filename = f"AS{level}U{unit:02}L{lesson:02}-story.jpg"
+                #             # Extract the JPG file from the zip
+                #             zip_object.extract(file_info, output_path)
                 # Delete the zip file when finished
                 os.remove(os.path.join(output_path, zip_filename))
         # stop after one unit
